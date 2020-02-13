d33f6f0f9b403b798952de3e34d68df8c3de43c7
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-13 18:37:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,7 @@ def get_japanese_emoticon(file_path, emoticon)
   japanese_emoticon ? japanese_emoticon : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
+  
 end
